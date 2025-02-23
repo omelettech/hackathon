de0b7f476e36a453fef3d233418045b2e5831508
Sokmontrey <sythatsokmontrey@gmail.com>
2025-02-23 05:51:27 -0500

    feat: feed

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,23 +1,26 @@
 ﻿export interface Item {
-    id: number;
-    title: string;
-    content: string;
-    imageUrl: string;
+    price: number;
+    expirationDate: string;
+    productName: string;
+    quality: string;
+    imgUrl: string;
 }
 
-export default function ItemCard({id, title, content, imageUrl}: Item) {
-    const downloadImage = (id: string) => {
-        return id:
-    };
+export default function ItemCard({
+    imgUrl,
+    productName,
+    price,
+    expirationDate,
+    quality}: Item) {
 
     return (
-        <div className="card" key={id}>
-            <img src={imageUrl} alt={title} className="card-image"/>
-            <img src={downloadImage(item.$id)} height={100} width={100} alt={item.title}
-                 className="card-image"/>
+        <div className="card d-flex flex-column align-items-center">
+            <img src={imgUrl} alt={productName} className="card-image" width={100} />
             <div className="card-content">
-                <h3 className="card-title">{title}</h3>
-                <p className="card-text">{content}</p>
+                <h3 className="card-title">{productName}</h3>
+                <p className="card-text">Price: ${price}</p>
+                <p className="card-text">Expiration Date: {expirationDate}</p>
+                <p className="card-text">Quality: {quality}</p>
             </div>
         </div>
     );
