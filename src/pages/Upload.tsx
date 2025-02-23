f8a05ebec8752a32bde33ffb13f08bd63d365b8b
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-02-23 08:42:47 -0500

    finalest

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -1,9 +1,7 @@
-import React, {useCallback, useEffect, useState} from "react";
-import { Client, Storage } from "appwrite";
-import {c} from "vite/dist/node/moduleRunnerTransport.d-CXw_Ws6P";
+import React, {useState} from "react";
+import {Client, Storage} from "appwrite";
 import {addDoc, collection} from 'firebase/firestore'
 import {db} from "../firebase/firebaseInit.tsx";
-import {getAuth} from "firebase/auth";
 import {useLocation} from "react-router-dom";
 import {sendImageToModel} from "../utils/ImageHandling.ts";
 import FloatingButton from "../components/FloatingButton.tsx";
@@ -13,36 +11,34 @@ const storage = new Storage(client);
 const BUCKET_ID = '67b9ec75001b90f35dbe';
 
 const Upload = () => {
-    const location = useLocation();
     const [uploading, setUploading] = useState(false);
     const [uploaded, setUploaded] = useState(false);
     const [message, setMessage] = useState("");
-    const { e } = location.state || {}; // Handle undefined case
 
     const onFileSelected = async (file: File) => {
         try {
-            const response = await storage.createFile(BUCKET_ID, file.name, file);
-        const url = storage.getFilePreview(BUCKET_ID,response.$id)
+            const response = await storage.createFile(BUCKET_ID, file.name + "__" + Date.now(), file);
+            const url = storage.getFilePreview(BUCKET_ID, response.$id)
 
-        console.log("Sending to model");
-        setUploading(true);
-        const data = await sendImageToModel(url)
+            console.log("Sending to model");
+            setUploading(true);
+            const data = await sendImageToModel(url)
 
-        const confirmed = confirm(`Estimated expiration date: ${data.expiration_day}, Price: ${data.price}`);
+            const confirmed = confirm(`Estimated expiration date: ${data.expiration_day}, Price: ${data.price}`);
 
-        if (!confirmed) return;
+            if (!confirmed) return;
 
-        const docRef = await addDoc(collection(db, "Posts"),
-        {
-            expirationDate: data.expiration_day,
-            imgUrl: url,
-            price: data.price,
-            quality: data.quality_name
-        });
+            const docRef = await addDoc(collection(db, "Posts"),
+                {
+                    expirationDate: data.expiration_day,
+                    imgUrl: url,
+                    price: data.price,
+                    quality: data.quality_name
+                });
 
         } catch (e) {
             console.error(e)
-        }finally {
+        } finally {
             setUploading(false);
             setUploaded(true);
         }
@@ -60,7 +56,6 @@ const Upload = () => {
                     <FloatingButton onFileSelected={onFileSelected}/>
 
                     <h2>Upload File</h2>
-                    <p>{e}</p>
 
                 </>}
             {message && <p>{message}</p>}
