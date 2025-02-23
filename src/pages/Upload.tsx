fa2c6d8018e0a0ebab15f2d16c8ec2bc2065fa2c
Awsaf Fida Mahmud <fidaawsaf@gmail.com>
2025-02-23 02:02:05 -0500

    Improved upload function

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { Client, Storage } from "appwrite";
 import {c} from "vite/dist/node/moduleRunnerTransport.d-CXw_Ws6P";
 import {addDoc, collection} from 'firebase/firestore'
 import {db} from "../firebase/firebaseInit.tsx";
 import {getAuth} from "firebase/auth";
+import {useLocation} from "react-router-dom";
+import {sendImageToModel} from "../utils/ImageHandling.ts";
+import FloatingButton from "../components/FloatingButton.tsx";
 
 const client = new Client().setProject("67b9e650002a4536f7db");
 const storage = new Storage(client);
@@ -12,11 +15,27 @@ const BUCKET_ID = '67b9ec75001b90f35dbe';
 
 
 const Upload = () => {
-    const [file, setFile] = useState<any | null>(null);
+    const location = useLocation();
+
+    const [file,setFile]=useState(null)
     const [uploading, setUploading] = useState(false);
-    const [progress, setProgress] = useState(0);
     const [message, setMessage] = useState("");
     const [productName, setproductName] = useState<string>("")
+    const { e } = location.state || {}; // Handle undefined case
+
+    useEffect(() => {
+        const handleImageAnalysis = async () =>{
+            console.log("Sending...")
+            const response = sendImageToModel(file)
+
+            response.then(r=>console.log(r))
+        }
+        if (file){
+            handleImageAnalysis()
+
+        }
+
+    }, [file]);
 
     const user = getAuth();
     const addData = async (qul_list: string[], qunt:number, img_url: string) =>{
@@ -34,30 +53,30 @@ const Upload = () => {
         }
     }
 
-
-
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if(event.target?.files[0] === null || event.target?.files[0] === undefined){
             setMessage("Something went wrong")
         }
         if (event.target.files && event.target.files.length > 0) {
-            setFile(event.target?.files[0]);
+            // setFile(event.target?.files[0]);
+
         } else {
             setMessage("No file selected");
         }
     };
 
     const handleUpload = async () => {
-        if (!file) return;
+        // if (!file) return;
 
         setUploading(true);
-        setProgress(0);
         setMessage("");
 
         try {
-            const response = await storage.createFile(BUCKET_ID, file.name, file);
-            {addData(["testone", "testtwo"], 3, response.$id)}
-            setMessage("File uploaded successfully!");
+            const response = await sendImageToModel(e)
+            // const qualityList = response.quality
+            // {addData([], 3, params)}
+            console.log("imagemodel response",response)
+
         } catch (error) {
             setMessage("Failed to upload file.");
         } finally {
@@ -67,10 +86,13 @@ const Upload = () => {
 
     return (
         <div className="upload-container">
+            <FloatingButton setFile={setFile}/>
+
             <h2>Upload File</h2>
-            <input type="file" onChange={handleFileChange} />
-            <button onClick={handleUpload} disabled={uploading || !file}>
-                {uploading ? `Uploading ${progress}%` : "Upload"}
+            <p>{e}</p>
+            {/*<input type="file" onChange={handleFileChange} />*/}
+            <button onClick={handleUpload} disabled={uploading || !message}>
+                {uploading ? `Uploading` : "Upload"}
             </button>
             <input type="text" onChange={(event)=>setproductName(event.target.value)} />
 
